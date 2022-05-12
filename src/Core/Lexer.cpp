2844365b71aefa82e530e575cf1fa28f0d14445e
George Tarkhnishvili <giorgi.tarkhnishvili123@gmail.com>
2022-05-13 00:41:13 +0400

    Added simple token types (to be enhanced)

--- src/Core/Lexer.cpp
+++ src/Core/Lexer.cpp
@@ -1,14 +1,25 @@
 #include "Lexer.hpp"
 
-LEX_RESULT lex(const std::vector<TOKEN>& tokens)
+LEX_RESULT process_tokens(const std::vector<TOKEN>& tokens)
 {
-	LEX_RESULT result = {
-		SUCCESS,
-		{},
-		{}
-	};
+	for (const TOKEN& token : tokens)
+	{
+		
+	}
+	return {};	
+}
 
-	
-	
-	return result;
+LEX_RESULT merge_processed_tokens(const std::vector<PROCESSED_TOKEN>& processed_tokens)
+{
+	return {};
+}
+
+LEX_RESULT lex(const std::vector<TOKEN>& tokens)
+{	
+	LEX_RESULT processing_result = process_tokens(tokens);
+	if (processing_result.status == FAIL) 
+	{
+		return processing_result;
+	}	
+	return merge_processed_tokens(processing_result.processed_tokens);
 }
--- src/Core/Utils.hpp
+++ src/Core/Utils.hpp
@@ -5,12 +5,6 @@
 
 typedef uint32_t uint;
 
-inline bool is_operator(const char c)
-{
-	return c == '+' || c == '-' || c == '*' || c == '/' || c == '%' ||
-		   c == '&' || c == '|' || c == '^';
-}
-
 inline bool is_space(const char c)
 {
 	unsigned char uc = static_cast<unsigned char>(c);
@@ -20,43 +14,11 @@ inline bool is_space(const char c)
 inline bool is_splittable(const char c)
 {
 	return c == '+' || c == '-' || c == '*' || c == '/' || c == '%' ||
-		   c == '&' || c == '|' || c == '^' ||
+		   c == '&' || c == '|' || c == '^' || c == '!' ||
 		   c == '>' || c == '<' || c == '=' ||
 		   c == '?' || c == ':' || 
 		   c == '(' || c == ')' || c == '{' || c == '}'	|| c == '[' || c == ']' ||
 		   c == ',' || c == ';';
-}
-
-inline bool is_datatype(const std::string& s)
-{
-	return s == "int" || s == "long" || s == "bigint" ||
-		   s == "char" || s == "string" ||
-		   s == "bool";
-}
-
-inline bool is_string(const std::string& s)
-{
-	return s.length() >= 2 && s.front() == '"' && s.back() == '"';
-}
-
-inline bool is_char(const std::string& s)
-{
-	return s.length() >= 2 && s.front() == '\'' && s.back() == '\'';
-}
-
-inline bool is_bool(const std::string& s)
-{
-	return s == "true" || s == "false";
-}
-
-inline bool is_number(const std::string& s)
-{
-	if (s.length() == 0) return false;
-	for (int i = 0; i < s.length(); i++)
-	{
-		if ((s[i] <= '0' || s[i] >= '9') && s[i] != '_') return false;
-	}
-	return true;
 }
 
 #endif
